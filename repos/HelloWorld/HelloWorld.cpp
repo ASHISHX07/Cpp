e997025b0ec0920f3de8685c1d502137c31d5c54
ASHX7 <ashishsindhav02@gmail.com>
2025-09-23 10:02:56 +0530

    Initial commit
    this commit is made from office laptop

---
+++ repos/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter a Number: ";
+
+	int c{};
+	std::cin >> c;
+
+	std::cout << "enter second Number: ";
+
+	int f{};
+	std::cin >> f;
+
+	/*std::cout << "enter Operation sign: ";*/
+
+	int x{};
+	std::cout << "first value: " << c << '\n' << "second value: " << f << '\n';
+
+	return 0;
+}
